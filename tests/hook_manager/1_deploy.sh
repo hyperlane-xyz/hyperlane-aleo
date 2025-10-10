bf7a301ec4556db683b0d47e539c258645f90c1c
Jamin <57451149+yjamin@users.noreply.github.com>
2025-10-10 12:04:14 +0200

    test: merkle tree hook (#10)

---
+++ tests/hook_manager/1_deploy.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+cd ./hook_manager
+output=$(leo deploy --broadcast -y)
+exit_code=$?
+echo "$output"
+if echo "$output" | grep -Eq '❌|Transaction rejected' ; then
+    exit 1
+fi
+exit $exit_code
