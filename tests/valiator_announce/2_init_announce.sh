72c044e7514cb1c95312511a149ccf1483fe105b
Jamin <kaminund@gmail.com>
2025-10-10 11:58:29 +0200

    test: validator announce

---
+++ tests/valiator_announce/2_init_announce.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+cd ./validator_announce
+output=$(leo execute init '[0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 243u8, 159u8, 214u8, 229u8, 26u8, 173u8, 136u8, 246u8, 244u8, 206u8, 106u8, 184u8, 130u8, 114u8, 121u8, 207u8, 255u8, 185u8, 34u8, 102u8]' 1337u32 --broadcast -y)
+exit_code=$?
+echo "$output"
+if echo "$output" | grep -Eq '❌|Transaction rejected' ; then
+    exit 1
+fi
+exit $exit_code
