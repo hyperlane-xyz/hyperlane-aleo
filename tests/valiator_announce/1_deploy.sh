0f07915a3154cb492bae0f8ed768648261320d89
Jamin <kaminund@gmail.com>
2025-10-15 14:14:04 +0200

    refactor: validator_announce test

--- tests/valiator_announce/1_deploy.sh
+++ tests/valiator_announce/1_deploy.sh
@@ -1,9 +1,4 @@
 #/bin/sh
+. "$(cd "$(dirname "$0")" && pwd)/../helpers.sh"
 cd ./validator_announce
-output=$(leo deploy --broadcast -y)
-exit_code=$?
-echo "$output"
-if echo "$output" | grep -Eq '❌|Transaction rejected' ; then
-    exit 1
-fi
-exit $exit_code
+transact leo deploy || exit 1
