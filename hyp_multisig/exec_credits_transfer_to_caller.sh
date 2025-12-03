b5102241144bcbcf9a2085fe39286cb2958738d5
Eran Rundstein <eran@rundste.in>
2025-12-03 10:24:30 -0800

    differentiate between wallet_id used for upgrades and wallet id used for privileged operations

--- hyp_multisig/exec_credits_transfer_to_caller.sh
+++ hyp_multisig/exec_credits_transfer_to_caller.sh
@@ -1,19 +1,16 @@
 set -e
 
-LEO=~/Projects/aleo/leo/target/release/leo
-
+WALLET_ID=$($LEO execute --skip-execute-proof --yes hyp_multisig.aleo/wallet_id | grep '• aleo' | awk '{print $2}')
 SIGNING_OP_ID=$($LEO execute --skip-execute-proof --yes hyp_multisig.aleo/nonce_to_signing_op_id ${RANDOM}u32 | grep field | awk '{print $2}')
 BLOCK_EXPIRATION=10u32
 echo "Signing op id: $SIGNING_OP_ID"
 
-#HYP_MULTISIG_ADDR=$($LEO execute --skip-execute-proof --yes hyp_multisig.aleo/get_self_address | grep " • aleo" | awk '{print $2}')
-#echo "Hyp multisig address: $HYP_MULTISIG_ADDR"
 
 AMOUNT=5
 
 $LEO execute --skip-execute-proof --yes --broadcast hyp_multisig.aleo/init_multisig_op $SIGNING_OP_ID $BLOCK_EXPIRATION "{op: 30u8, arg_addr_0: aleo1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq3ljyzc, arg_addr_1: aleo1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq3ljyzc, arg_addr_2: aleo1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq3ljyzc, arg_addr_3: aleo1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq3ljyzc, arg_u128_0: ${AMOUNT}u128, arg_u128_1: 0u128, arg_u128_2: 0u128, arg_u128_3: 0u128}"
 
-PRIVATE_KEY=APrivateKey1zkp2RWGDcde3efb89rjhME1VYA8QMxcxep5DShNBR6n8Yjh $LEO execute --skip-execute-proof --yes --broadcast multisig_core.aleo/sign hyp_multisig.aleo $SIGNING_OP_ID
+PRIVATE_KEY=APrivateKey1zkp2RWGDcde3efb89rjhME1VYA8QMxcxep5DShNBR6n8Yjh $LEO execute --skip-execute-proof --yes --broadcast multisig_core.aleo/sign $WALLET_ID $SIGNING_OP_ID
 
 
 $LEO execute --skip-execute-proof --yes --broadcast hyp_multisig.aleo/exec_credits_transfer_to_caller $SIGNING_OP_ID ${AMOUNT}u64
---
+++ hyp_multisig/exec_mailbox_set_owner.sh
@@ -0,0 +1,17 @@
+set -e
+
+WALLET_ID=$($LEO execute --skip-execute-proof --yes hyp_multisig.aleo/wallet_id | grep '• aleo' | awk '{print $2}')
+SIGNING_OP_ID=$($LEO execute --skip-execute-proof --yes hyp_multisig.aleo/nonce_to_signing_op_id ${RANDOM}u32 | grep field | awk '{print $2}')
+BLOCK_EXPIRATION=10u32
+echo "Signing op id: $SIGNING_OP_ID"
+
+
+NEW_ADDR=aleo1rhgdu77hgyqd3xjj8ucu3jj9r2krwz6mnzyd80gncr5fxcwlh5rsvzp9px
+echo "New address: $NEW_ADDR"
+
+$LEO execute --skip-execute-proof --yes --broadcast hyp_multisig.aleo/init_multisig_op $SIGNING_OP_ID $BLOCK_EXPIRATION "{op: 2u8, arg_addr_0: $NEW_ADDR, arg_addr_1: aleo1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq3ljyzc, arg_addr_2: aleo1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq3ljyzc, arg_addr_3: aleo1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq3ljyzc, arg_u128_0: 0u128, arg_u128_1: 0u128, arg_u128_2: 0u128, arg_u128_3: 0u128}"
+
+PRIVATE_KEY=APrivateKey1zkp2RWGDcde3efb89rjhME1VYA8QMxcxep5DShNBR6n8Yjh $LEO execute --skip-execute-proof --yes --broadcast multisig_core.aleo/sign $WALLET_ID $SIGNING_OP_ID
+
+
+$LEO execute --skip-execute-proof --yes --broadcast hyp_multisig.aleo/exec_mailbox_set_owner $SIGNING_OP_ID $NEW_ADDR
